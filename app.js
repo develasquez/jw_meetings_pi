cc6fd03d3c77160e645b4a9818f5e24f60865264
Felipe Velasquez <felipe.velasquez@globant.com>
2018-10-23 18:42:15 -0300

    enable cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*, file");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
